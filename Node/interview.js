15b1895fb3e29dfa7989946d2efd710c8948b977
ironhideee <492948452@qq.com>
2019-10-11 11:48:51 +0800

    promise test

---
+++ Node/interview.js
@@ -0,0 +1,28 @@
+
+    var promise = interview(1)
+        .then(()=>{
+            return interview(2)
+        })
+        .then(()=>{
+            return interview(3)
+        })
+        .then(()=>{
+            console.log('smile');
+        })
+        .catch((err)=>{
+            console.log('cry at '  + err.round + ' round');
+        });
+
+    function interview(round){
+        return new Promise((resolve,reject) => {
+            setTimeout(()=>{
+                if(Math.random() > 0.2){
+                    resolve('success')
+                }else {
+                    var error = new Error('fail');
+                    error.round = round;
+                    reject(error);
+                }
+            },500)
+        })
+    }
